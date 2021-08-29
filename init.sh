ca26dfd843f9fe192761fee12b8a85e58e13eaec
mrprizmo <70138944+mrprizmo@users.noreply.github.com>
2021-08-29 22:50:25 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,3 +3,5 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 sudo gunicorn -b 0.0.0.0:8080 hello:app
+sudo curl -vv 'http://127.0.0.1/hello/?a=1&a=2&b=3'
+sudo curl -vv 'http://127.0.0.1:8080/?a=1&a=2&b=3'
