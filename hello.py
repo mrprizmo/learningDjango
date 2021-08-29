987e7f682687e89114345d331b6e898f97737ab4
mrprizmo <anton.dorosev@yandex.ru>
2021-08-29 14:53:36 +0300

    round 2 fourth

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def app(environ, start_response):
     data = [bytes(line + '\n', 'ascii') for line in environ["QUERY_STRING"].split('&')]
-    start_response("200 OK", ("Content-Type", "text/plain"))
-    return iter(data)
+    start_response("200 OK", [("Content-Type", "text/plain")])
+    return datagit
