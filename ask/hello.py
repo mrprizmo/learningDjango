56de013f0d8d83858dbfb8ec0bb3c7bfa03720ad
mrprizmo <anton.dorosev@yandex.ru>
2021-08-30 15:40:56 +0300

    round 3 nineth

--- ask/hello.py
+++
@@ -1,4 +0,0 @@
-def app(environ, start_response):
-    data = [bytes(line + '\n', 'ascii') for line in environ["QUERY_STRING"].split('&')]
-    start_response("200 OK", [("Content-Type", "text/plain")])
-    return data
