9a617d3574423b233d7321686f09032d95fdae94
mrprizmo <anton.dorosev@yandex.ru>
2021-08-30 15:55:35 +0300

    round 3 tenth

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t*v13xx03)fo+zzzq(df#^n$)yw!w9(ihx5gu3h4#*6xd_e#8v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
