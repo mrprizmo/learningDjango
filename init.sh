03e338bf9b5fdb48570d6d0b74fde1189644fa70
mrprizmo <anton.dorosev@yandex.ru>
2021-08-30 14:15:35 +0300

    round 3 second

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo pip3 install --upgrade pip
-sudo pip3 install --upgrade django
-sudo pip3 install --upgrade gunicorn
+sudo pip install --upgrade pip
+sudo pip install --upgrade django
+sudo pip install --upgrade gunicorn
 sudo ln -sf /home/box/web/etc/django_conf.py /etc/init.d/gunicorn.d/django_conf.py
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn --bind 0.0.0.0:8080 ask.wsgi:application
+sudo gunicorn --bind 0.0.0.0:8080 ask.ask.wsgi:application
