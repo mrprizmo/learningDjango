86b5f6650747bbd56645dfcbb7d0e248b5d64ae5
mrprizmo <anton.dorosev@yandex.ru>
2021-08-30 11:27:02 +0300

    local chenges

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
-subo gunicorn -b 0.0.0.0:8080 hello:app
+sudo gunicorn -b 0.0.0.0:8080 hello:app
