427d908aa9b2e8a24801d4265a690279bd082335
mrprizmo <anton.dorosev@yandex.ru>
2021-08-30 15:36:24 +0300

    round 3 eighth

--- ask/init.sh
+++ ask/init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn --bind 0.0.0.0:8080 ask.wsgi:application
+sudo gunicorn --bind 0.0.0.0:8080 hello:app
+sudo gunicorn --bind 0.0.0.0:8000 ask.wsgi:application
