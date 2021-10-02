24b815f5011463dddab2a9bc3f0a5f08c321f775
mrprizmo <anton.dorosev@yandex.ru>
2021-10-02 23:30:13 +0300

    t1 3

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ask'
+    "qa.models.Question",
+    "qa.models.Answer"
 ]
 
 MIDDLEWARE = [
